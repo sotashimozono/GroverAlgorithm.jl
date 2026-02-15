974bec2e57712626e90e4704283339b226347ca0
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-02-15 23:29:19 +0900

    一通りはできた

--- src/GroverAlgorithm.jl
+++ src/GroverAlgorithm.jl
@@ -1,7 +1,10 @@
 module GroverAlgorithm
 
+using ITensors, ITensorMPS
 using LaTeXStrings, TikzPictures
+
 include("core/abstractquantumgate.jl")
+include("core/convert_itensorgate.jl")
 include("core/latexstrings.jl")
 include("core/convert_quantikz.jl")
 
