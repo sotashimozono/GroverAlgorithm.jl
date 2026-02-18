ca1aea4b5006e7fee9b5f8ba33ffbe8c1fd6cf9a
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-02-18 18:56:30 +0900

    一旦戻した

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ makedocs(;
         canonical="https://codes.sota-shimozono.com/GroverAlgorithm.jl/stable/",
         prettyurls=get(ENV, "CI", "false") == "true",
         ansicolor=true,
+        themes = ["light", "dark"],
         mathengine=MathJax3(
             Dict(
                 :tex => Dict(
