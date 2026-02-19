736efce3fed8147917b58a42a3557da463ef5065
Souta <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-02-19 14:36:49 +0900

    test code moved

--- src/GroverAlgorithm.jl
+++ src/GroverAlgorithm.jl
@@ -3,11 +3,13 @@ module GroverAlgorithm
 using ITensors, ITensorMPS
 using LaTeXStrings, TikzPictures
 
-include("core/initialstate.jl")
-include("core/measurement.jl")
 include("core/abstractquantumgate.jl")
-include("core/convert_itensorgate.jl")
-include("core/latexstrings.jl")
-include("core/convert_quantikz.jl")
+include("ITensorIO/measurement.jl")
+
+include("ITensorIO/initialstate.jl")
+include("ITensorIO/convert_itensorgate.jl")
+
+include("QuantikzIO/latexstrings.jl")
+include("QuantikzIO/convert_quantikz.jl")
 
 end # module GroverAlgorithm
